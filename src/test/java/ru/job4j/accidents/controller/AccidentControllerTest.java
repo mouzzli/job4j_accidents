47cdcb965288a78b26afe44f22236bf4552e6dac
Mouzzli <cokes03@yandex.ru>
2023-04-12 22:20:12 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -1,16 +1,29 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import ru.job4j.accidents.Application;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
@@ -18,6 +31,8 @@ class AccidentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private AccidentService accidentService;
 
     @Test
     @WithMockUser
@@ -32,9 +47,48 @@ class AccidentControllerTest {
     @Test
     @WithMockUser
     public void whenViewUpdateAccident() throws Exception {
+        when(accidentService.findById(1)).thenReturn(Optional.of(new Accident()));
         this.mockMvc.perform(get("/accidents/updateForm?id=1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("accident/editAccident"))
                 .andExpect(model().attributeExists("user", "types", "rules", "accident"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenSave() throws Exception {
+        List<Integer> rIds = Arrays.asList(1, 2, 3);
+        Accident accident = new Accident();
+        accident.setNumber("123");
+        this.mockMvc.perform(post("/accidents/create")
+                        .flashAttr("accident", accident)
+                        .param("rIds", "1")
+                        .param("rIds", "2")
+                        .param("rIds", "3"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService, times(1)).save(argument.capture(), eq(rIds));
+        assertThat(argument.getValue().getNumber()).isEqualTo("123");
+    }
+
+    @Test
+    @WithMockUser
+    public void whenUpdate() throws Exception {
+        List<Integer> rIds = Arrays.asList(1, 2, 3);
+        Accident accident = new Accident();
+        accident.setNumber("123");
+        this.mockMvc.perform(post("/accidents/update")
+                        .flashAttr("accident", accident)
+                        .param("rIds", "1")
+                        .param("rIds", "2")
+                        .param("rIds", "3"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService, times(1)).update(argument.capture(), eq(rIds));
+        assertThat(argument.getValue().getNumber()).isEqualTo("123");
     }
 }
--- src/test/java/ru/job4j/accidents/controller/UserControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/UserControllerTest.java
@@ -1,13 +1,21 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Application;
+import ru.job4j.accidents.model.User;
+import ru.job4j.accidents.service.UserService;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = Application.class)
@@ -15,6 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
 
     @Test
     public void whenLoginPage() throws Exception {
@@ -37,5 +48,20 @@ class UserControllerTest {
         this.mockMvc.perform(get("/logout"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/users/login?logout=true"));
+    }
+
+    @Test
+    public void whenRegSave() throws Exception {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("testPassword");
+        this.mockMvc.perform(post("/users/reg")
+                        .flashAttr("user", user))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/login"));
+
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userService, times(1)).save(argument.capture());
+        assertThat(argument.getValue()).isEqualTo(user);
     }
 }
